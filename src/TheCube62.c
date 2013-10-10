ff3a61b96c8d39baab6de7f34fe487f58bd40a7e
Gon Nido <insectopalo@gmail.com>
2013-10-10 13:05:37 +1300

    config.h file added

--- src/TheCube62.c
+++ src/TheCube62.c
@@ -4,19 +4,7 @@
 #include <time.h>
 #include <string.h>
 #include <unistd.h>
-
-/* Example sequences
-   1) Real Pascu cube:
-   int Sequence[] = {3,2,3,2,2,4,2,3,2,3,2,3,2,2,2,2,2,2,2,2,3,3,2,2,2,2,2,3,4,2,2,2,4,2,3,2,2,2,2,2,2,2,2,2,4,2};
-   2) Shorter 4x4x4 sequence
-   int Sequence[] = {4,4,4,3,3,2,2,2,3,2,4,4,4,2,3,2,2,2,3,3,4,4,4,2,4,4,4,3,3,2,2};
-   3) 3x3x3 cube
-   int Sequence[] = {3,3,3,2,2,2,2,2,3,3,3,2,3,3,3,2,2};
-   4) 3x3x3 cube from http://www.mathematische-basteleien.de/snakecube.htm
-   int Sequence[] = {3,2,2,3,2,3,2,2,3,3,2,2,2,3,3,3,3};
-*/
-
-   int Sequence[] = {3,2,3,2,2,4,2,3,2,3,2,3,2,2,2,2,2,2,2,2,3,3,2,2,2,2,2,3,4,2,2,2,4,2,3,2,2,2,2,2,2,2,2,2,4,2};
+#include "config.h"
 
 struct globalArgs_t {
   int output;                 /* -o option */
